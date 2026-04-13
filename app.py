5a7da904fcd95befa33485aaf15c720bee1b434c
Siva290395 <129871285+Siva290395@users.noreply.github.com>
2026-04-13 14:46:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,6 @@ class Handler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.end_headers()
-        self.wfile.write(b"Hello from Jenkins + Kubernetes! v3")
+        self.wfile.write(b"Hello from Jenkins + Kubernetes! v1")
 
 HTTPServer(("", 8080), Handler).serve_forever()
